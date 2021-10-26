55cd0870305456f4efccecbbe992b35d8cfc43a2
haerulmutakin <haerulm.takin@gmail.com>
2021-10-26 07:54:17 +0700

    episode 5: Event handlers

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,17 @@
 const Home = () => {
+    const handleClick = (e) => {
+        console.log('Halo', e);
+    }
+
+    const handleClicklagi = (name, e) => {
+        console.log('Halo', name);
+        console.log(e);
+    }
     return ( 
         <div className="home">
             <h3>Contact List</h3>
+            <button onClick={handleClick}>Halo</button>
+            <button onClick={(e) => handleClicklagi('Luffy', e)}>Halo lagi</button>
         </div>
      );
 }
