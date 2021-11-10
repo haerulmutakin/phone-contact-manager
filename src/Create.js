ec8f7450e1e264b5e64743a28f45c00f9425d8de
haerulmutakin <haerulm.takin@gmail.com>
2021-11-11 05:43:16 +0700

    episode 16: handle from submit event

--- src/Create.js
+++ src/Create.js
@@ -1,7 +1,43 @@
+import { useState } from 'react';
+
 const Create = () => {
+    const [name, setName] = useState('');
+    const [number, setNumber] = useState('');
+
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const contact = {name, number};
+        console.log(contact);
+    }
+
     return ( 
         <div className="contact-form">
             <h3>Contact Form</h3>
+            <form onSubmit={handleSubmit}>
+                <div className="form-group">
+                    <label className="control-label">Contact name</label>
+                    <input 
+                        type="text"
+                        className="form-control"
+                        value={name}
+                        onChange={(e) => setName(e.target.value)}
+                    />
+                </div>
+                <div className="form-group">
+                    <label className="control-label">Contact number</label>
+                    <input 
+                        type="number"
+                        className="form-control"
+                        value={number}
+                        onChange={(e) => setNumber(e.target.value)}
+                    />
+                </div>
+                <div className="btn-group">
+                    <button type="button" className="btn btn-danger">Cancel</button>
+                    <button type="submit" className="btn btn-primary">Submit</button>
+                </div>
+            </form>
         </div>
      );
 }
