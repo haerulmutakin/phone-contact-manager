e0cd9987c89f04523387253b2500c54e1a1257de
haerulmutakin <haerulm.takin@gmail.com>
2021-11-11 14:10:22 +0700

    episode 17: making a POST request to server

--- src/Create.js
+++ src/Create.js
@@ -1,14 +1,23 @@
 import { useState } from 'react';
+import { useHistory } from 'react-router-dom';
+import Api from './api/contact-api';
 
 const Create = () => {
+    const browserHistory = useHistory();
     const [name, setName] = useState('');
     const [number, setNumber] = useState('');
+    const [loading, setLoading] = useState(false);
 
 
     const handleSubmit = (e) => {
         e.preventDefault();
         const contact = {name, number};
-        console.log(contact);
+
+        setLoading(true);
+        Api.post('/contacts', contact).then(() => {
+            setLoading(false);
+            browserHistory.goBack();
+        });
     }
 
     return ( 
@@ -34,8 +43,10 @@ const Create = () => {
                     />
                 </div>
                 <div className="btn-group">
-                    <button type="button" className="btn btn-danger">Cancel</button>
-                    <button type="submit" className="btn btn-primary">Submit</button>
+                    <button type="button" className="btn btn-danger" onClick={() => browserHistory.goBack()}>Cancel</button>
+                    <button type="submit" className="btn btn-primary">
+                        {loading ? 'Submitting...' : 'Submit'}
+                    </button>
                 </div>
             </form>
         </div>
