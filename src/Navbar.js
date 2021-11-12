e4fbf9d91ef56852673d9b7379482002d23c3602
haerulmutakin <haerulm.takin@gmail.com>
2021-11-12 09:34:32 +0700

    episode 18: activate menu

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,12 +1,12 @@
-import {Link} from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 const Navbar = () => {
     return ( 
         <nav>
             <h1>Phone Contact</h1>
             <div className="nav-menu">
-                <Link to="/">Home</Link>
-                <Link to="/create">New Contact</Link>
+                <NavLink to="/" exact activeClassName="active">Home</NavLink>
+                <NavLink to="/create" activeClassName="active">New Contact</NavLink>
             </div>
         </nav>
      );
